ada8d1e900ebd89504a5b7df2f80e4ee3d1fd8a6
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-12-26 18:22:10 +0000

    Add main header file

---
+++ rusC.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define печатать printf
+#define числовой int
+#define пока while
+#define правда 1
+#define ложь 0
+#define главный main
+#define равно ==
+#define если if
+#define иначе else
+#define вернуть return
+#define символ char
